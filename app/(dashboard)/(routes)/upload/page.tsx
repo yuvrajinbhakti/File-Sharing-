3835bdbc549e74b37b421ab9d848bfd9fbfc42f0
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-01-05 22:03:34 +0530

    Added File upload ui

--- app/(dashboard)/(routes)/upload/page.tsx
+++ app/(dashboard)/(routes)/upload/page.tsx
@@ -1,7 +1,17 @@
+"use client";
 import React from "react";
+import UploadForm from "./_components/UploadForm";
 
 const Upload = () => {
-  return <div>upload</div>;
+  return (
+    <div className="p-5 px-8 md:px-20">
+      <h2 className="text-[20px] text-center m-5">
+        Start <strong className="text-primary"> Uploading</strong> Files and
+        <strong className="text-primary"> Share</strong> it
+      </h2>
+      <UploadForm />
+    </div>
+  );
 };
 
 export default Upload;
